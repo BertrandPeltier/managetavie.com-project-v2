da35462cec73df9ebc9b4c7d8c99089e98b9347a
Bob Moran <bertrandpeltier1979@gmail.com>
2022-06-13 13:55:45 +0200

    Création de l'effet lighted

--- src/views/Accueil/index.js
+++ src/views/Accueil/index.js
@@ -30,31 +30,34 @@ const Accueil = () => {
       </Container>
       <Navbar collapseOnSelect expand="lg" sticky="top">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Manage at Vie</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <Nav.Link href="#features">La méthode</Nav.Link>
+              <NavDropdown title="Accompagnements" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Happy'Culture</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Bee'Zen</NavDropdown.Item>
               </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
+              <Nav.Link href="#pricing">Manage ta Vie en entreprise</Nav.Link>
+              <Nav.Link href="#pricing">Contenus inspirants</Nav.Link>
+              <Nav.Link href="#pricing">Podcast</Nav.Link>
+              <Nav.Link href="#pricing">Ateliers</Nav.Link>
+              <Nav.Link href="#pricing">À propos</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Container ref={sectionRef} className="section-two">
-        <h1>Section 2</h1>
+        <Row className="bg-secondary-yellow text-center py-3">
+          <h1 className="title">Reprends les commandes<br />pour <span className="lighted--white">profitez de ta vie</span></h1>
+          <h2>pas demain, plus tard ou un jour peut-être !</h2>
+          <p>
+            grâce à une méthode qui a fait ses preuves sur plus de 150 entrepreneurs,
+            dirigeants et managers.
+          </p>
+        </Row>
       </Container>
       <Container className="section-three">
         <h1>Section 3</h1>
