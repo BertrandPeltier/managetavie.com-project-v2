4306f0264f61749190b3c2d5806603fc4362c22f
Bob Moran <bertrandpeltier1979@gmail.com>
2022-06-21 14:01:18 +0200

    début construction section2

--- src/views/Accueil/index.js
+++ src/views/Accueil/index.js
@@ -77,22 +77,40 @@ const Accueil = () => {
               <Nav.Link href="#pricing">Contenus inspirants</Nav.Link>
               <Nav.Link href="#pricing">Podcast</Nav.Link>
               <Nav.Link href="#pricing">Ateliers</Nav.Link>
-              <Nav.Link href="#pricing" onClick={() => console.log(window.scrollY)}>À propos</Nav.Link>
+              <Nav.Link href="#pricing">À propos</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container as="section" ref={sectionRef} className="section-two">
-        <Row as="div" className="section-two__gutterup" />
-        <Row className="bg-secondary-yellow text-center py-3">
-          <h1 className="title">Reprends les commandes<br />pour <span className="lighted--white">profitez de ta vie</span></h1>
-          <h2>pas demain, plus tard ou un jour peut-être !</h2>
-          <p>
-            grâce à une méthode qui a fait ses preuves sur plus de 150 entrepreneurs,
-            dirigeants et managers.
-          </p>
-        </Row>
-      </Container>
+      <section ref={sectionRef} className="section2">
+        <div className="gutter-up" />
+        <div className="main">
+          <Row className="main__part1 bg-secondary-yellow text-center py-5 mx-0">
+            <h1 className="text-title">Reprends les commandes<br />pour <span className="lighted--white">profitez de ta vie</span></h1>
+            <h2>pas demain, plus tard ou un jour peut-être !</h2>
+            <p>
+              grâce à une méthode qui a fait ses preuves sur plus de 150 entrepreneurs,
+              dirigeants et managers.
+            </p>
+          </Row>
+          <Row className="main__part2 bg-primary-blue text-center py-5 mx-0">
+            <Col as="ul" xs={12} md={4} className="text-title text-white fs-4">
+              <li>* Révèle ton plein potentiel</li>
+              <li>* Épanouis-toi dans ton job</li>
+              <li>* Préserve ton temps libre</li>
+              <li>* Et surtout, garde le sourire</li>
+            </Col>
+          </Row>
+          <Row className="main__part3 bg-secondary-light-blue text-center py-5 mx-0">
+            <Col as="ul" xs={12} md={4} className="text-title text-white fs-4">
+              <li>* Révèle ton plein potentiel</li>
+              <li>* Épanouis-toi dans ton job</li>
+              <li>* Préserve ton temps libre</li>
+              <li>* Et surtout, garde le sourire</li>
+            </Col>
+          </Row>
+        </div>
+      </section>
       <Container as="section" className="section-three">
         <h1>Section 3</h1>
       </Container>
