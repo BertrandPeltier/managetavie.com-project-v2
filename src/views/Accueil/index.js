8168a6c8b47c67c6c3427a4899567ebfed034d7b
BertrandPeltier <bertrandpeltier1979@gmail.com>
2022-07-18 21:51:46 +0200

    Rattrapage commit

--- src/views/Accueil/index.js
+++ src/views/Accueil/index.js
@@ -14,14 +14,14 @@ import './styles.scss';
 
 const Accueil = () => {
   // Scroll to
-  const sectionRef = useRef();
+  const section2 = useRef();
   const scrollToSection = (ref) => {
     window.scrollTo({ behavior: 'smooth', top: ref.current.offsetTop });
   };
 
   return (
     <div className="accueil">
-      <section className="section1">
+      <section className="section1  bg-primary-blue">
         <Container className="section1__main" as="main">
           <Row xs={1} md={2}>
             <Col>
@@ -38,7 +38,7 @@ const Accueil = () => {
               <Button
                 className="section1__main__action-button text-action"
                 variant="primary-yellow"
-                onClick={() => scrollToSection(sectionRef)}
+                onClick={() => scrollToSection(section2)}
               >
                 Bzzz c'est par ici !
               </Button>
@@ -51,7 +51,7 @@ const Accueil = () => {
             path={mdiChevronDown}
             size={2}
             aria-label="Scroll to next section"
-            onClick={() => scrollToSection(sectionRef)}
+            onClick={() => scrollToSection(section2)}
           />
           <div className="section1__footer__white-stripe" />
         </footer>
@@ -59,6 +59,66 @@ const Accueil = () => {
           className="section1__cloods"
           src={cloods}
         />
+      </section>
+      <Navbar collapseOnSelect expand="lg" sticky="top" className="bg-white">
+        <Container>
+          <Navbar.Brand href="#home">Manage at Vie</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">La méthode</Nav.Link>
+              <NavDropdown title="Accompagnements" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Happy'Culture</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">Bee'Zen</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#pricing">Manage ta Vie en entreprise</Nav.Link>
+              <Nav.Link href="#pricing">Contenus inspirants</Nav.Link>
+              <Nav.Link href="#pricing">Podcast</Nav.Link>
+              <Nav.Link href="#pricing">Ateliers</Nav.Link>
+              <Nav.Link href="#pricing">À propos</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <section ref={section2} className="section2">
+        <header className="section2__header" />
+        <main className="section2__main">
+          <div className="section2__main__part1 bg-secondary-yellow">
+            <Container>
+              <Row className="text-center py-5">
+                <h2 className="text-title">Reprends les commandes<br />pour <span className="lighted--white">profitez de ta vie</span></h2>
+                <h3 className="text-quote">pas demain, plus tard ou un jour peut-être !</h3>
+                <p>
+                  grâce à une méthode qui a fait ses preuves sur plus de 150 entrepreneurs,
+                  dirigeants et managers.
+                </p>
+              </Row>
+            </Container>
+          </div>
+          <div className="section2__main__part2 bg-primary-blue">
+            <Container>
+              <Row className="text-justify py-5">
+                <Col as="ul" className="text-title text-white fs-4">
+                  <li><Image src={checkbox} className="mx-3 mb-3" />Révèle ton plein potentiel</li>
+                  <li><Image src={checkbox} className="mx-3 mb-3" />Épanouis-toi dans ton job</li>
+                  <li><Image src={checkbox} className="mx-3 mb-3" />Préserve ton temps libre</li>
+                  <li><Image src={checkbox} className="mx-3 mb-3" />Et surtout, garde le sourire</li>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+          <Row className="section2__main__part3 bg-secondary-light-blue py-5 mx-0">
+            <Col xs={12} md={6} className="mx-3">
+              <h2 className="text-title">Bienvenue<br />dans l'univers Manage ta Vie</h2>
+              <h3 className="text-quote">Je suis Vanessa Rocherieux</h3>
+              <p>
+                Fondatrice de Manage ta Vie, coach, formatrice et sophrologue
+                spécialisée en bien-être Professionnel
+              </p>
+            </Col>
+          </Row>
+        </main>
       </section>
     </div>
   );
